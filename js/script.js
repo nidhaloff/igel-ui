0ad58e19d2cf5e5866ddf30fe9032c3e02d60b44
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-18 14:43:04 +0200

    basic functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,94 @@
 
-function getTrainingParams() {
+var path = require('path');
+var fs = require('fs');
+
+
+function getTrainingParams () {
     var trainDataPath = document.getElementById('train_data_path').files[0].path;
     var testSize = document.getElementById('test_size').value;
     var shuffle = document.getElementById('shuffle').checked;
     var missingValueOption = document.getElementById('missing-values').value;
-    console.log({
-        trainDataPath,
-        testSize,
-        shuffle,
-        missingValueOption
+    var encodingOption = document.getElementById('encoding').value;
+    var scalingMethod = document.getElementById('scale').value;
+    var scalingTarget = document.getElementById('scale-target').value;
+    var modelType = document.getElementById('model-type').value;
+    var algorithm = document.getElementById('algorithm').value;
+    var target = document.getElementById('target').value;
+    
+    var datasetOptions = {
+        type: "csv",
+        split: {
+            test_size: parseFloat(testSize),
+            shuffle: shuffle
+        },
+        preprocess: {
+            missing_values: missingValueOption,
+            encoding: {type: encodingOption},
+            scale: {
+                method: scalingMethod,
+                target: scalingTarget
+            }
+        }
+    }
+
+    var modelOptions = {
+        type: modelType,
+        algorithm: algorithm,
+
+    }
+    var trainingParams = JSON.stringify({
+        dataset: datasetOptions,
+        model: modelOptions,
+        target: [target]
+    }, null, 4);
+
+    console.log(`training parameters: ${trainingParams}`);
+    var currPath = path.join(__dirname, '../');
+    console.log("path => ", currPath);
+
+    fs.writeFile("test.json", trainingParams, function(err) {
+        if (err) {
+            console.log(err);
+        }
     });
 
+    const payload = {
+        cmd: "fit",
+        data_path: trainDataPath,
+        yaml_path: currPath + "test.json"
+    }
+    //console.log(`payload: ${JSON.stringify(payload)}`);
+    sendToPython(JSON.stringify(payload));
 
 }
 
+function getEvaluationParams () {
+    console.log("getting eval params");
+    let evalDataPath = document.getElementById('eval-data-path').files[0].path;
+    let payload = {
+        cmd: "evaluate",
+        data_path: evalDataPath
+    };
+    console.log(`evaluation payload: ${JSON.stringify(payload)}`);
+    sendToPython(JSON.stringify(payload));
+}
+
+function getPredictParams () {
+    alert("getting prediction params");
+    let predictDataPath = document.getElementById('pred-data-path').files[0].path;
+    let payload = {
+        cmd: "predict",
+        data_path: predictDataPath
+    };
+    alert(`evaluation payload: ${JSON.stringify(payload)}`);
+    sendToPython(JSON.stringify(payload));
+}
 
 function sendToPython(arg) {
     var python = require('python-shell');
-    var path = require('path')
+    var path = require('path');
 
-    var pyPath = path.join(__dirname, 'py/')
+    var pyPath = path.join(__dirname, '..', 'py/')
     console.log("pypath => ", pyPath)
     var options = {
         scriptPath: pyPath,
@@ -33,17 +102,3 @@ function sendToPython(arg) {
   
   }
 
-
-
-
-function send() {
-    var cmd = document.getElementById('cmd').value;
-    console.log("cmd: ", cmd);
-    const payload = {
-        cmd: cmd,
-        data_path: dataPath,
-        yaml_path: yamlPath
-    }
-    console.log(`payload: ${JSON.stringify(payload)}`);
-    sendToPython(JSON.stringify(payload));
-}
--- py/main.py
+++ py/main.py
@@ -7,8 +7,8 @@ arg = sys.argv[1]
 payload = json.loads(arg)
 print(f"Executing {payload['cmd']} command")
 print(f"path to the data \n {payload['data_path']}")
-print(f"path to the yaml file \n {payload['yaml_path']}")
+#print(f"path to the yaml file \n {payload['yaml_path']}")
 
 Igel(**payload)
 
-sys.stdout.flush()
+#sys.stdout.flush()
